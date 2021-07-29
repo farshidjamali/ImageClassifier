956e72a79aca835f8b74ab95586826e23184a879
Farshid Jamali <farshid.ae@gmail.com>
2021-07-29 16:12:58 -0400

    fastai not installing on my pc, i will try colab for now

---
+++ InstallingFastai.md
@@ -0,0 +1,7 @@
+#reference: https://docs.fast.ai/#Installing
+
+You can use fastai without any installation by using Google Colab.
+
+You can install fastai on your own machines with conda (highly recommended), as long as you're running Linux or Windows (NB: Mac is not supported).
+
+conda install -c fastchan fastai
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+# run the following line of code in terminal to install fastai.
+# pip install fastai
+install fastai
+import fastai
+help(fastai)
+
+
 #Importing libraries  
-pip install fastai
+'''pip install fastai
 import fastai  
 fastai.__version__  
 from fastai import *  
-from fastai.vision import *
+from fastai.vision import *  '''
